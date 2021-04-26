400a321584632d0cb99025f5a8fa1591368daae0
davidmedero <david.gregory.medero@gmail.com>
2021-04-26 16:30:44 -0400

    first push

---
+++ react-project/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Forever21 from "./components/Forever21";
+
+function App() {
+  return (
+    <div className="App">
+      <Forever21 />
+    </div>
+  );
+}
+
+export default App;
