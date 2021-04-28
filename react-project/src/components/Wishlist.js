c6b2d5382fd0891b6021c697dc88b6fdc61d477c
Jesper <jesperbennett@protonmail.com>
2021-04-28 19:51:21 -0400

    wasd

--- react-project/src/components/Wishlist.js
+++ react-project/src/components/Wishlist.js
@@ -2,79 +2,29 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Wishlist(props) {
-  let [fakestoreWishlist, setFakestoreWishlist] = useState([]);
-  let [makeupWishlist, setMakeupWishlist] = useState([]);
-  let [mergedWishlist, setMergedWishlist] = useState([]);
+  let [wishlist, setWishlist] = useState([]);
 
   useEffect(() => {
     axios.get(`https://ironrest.herokuapp.com/wishlist`).then((res) => {
-      setMakeupWishlist(res.data);
+      setWishlist(res.data);
     });
   }, []);
 
   function refreshMakeupPage() {
     axios.get(`https://ironrest.herokuapp.com/wishlist`).then((res) => {
-      setMakeupWishlist(res.data);
+      setWishlist(res.data);
     });
   }
 
-  // function mergeWishlists() {
-  //   // setMergedWishlist(
-  //   console.log(makeupWishlist);
-  //   makeupWishlist.map((item) => {
-  //     console.log(item);
-  //     return mergedWishlist.push({
-  //       name: item.product.name,
-  //       price: item.product.price,
-  //       image: item.product.image_link,
-  //       id: item._id,
-  //     });
-  //   });
 
-  //   fakestoreWishlist.map((item) => {
-  //     console.log(item);
-
-  //     return mergedWishlist.push({
-  //       name: item.product.title,
-  //       price: item.product.price,
-  //       image: item.product.image,
-  //       id: item._id,
-  //     });
-  //   });
-  //   console.log(mergedWishlist);
-  // }
-
-  let showProducts = () => {
-    // mergeWishlists();
-    return mergedWishlist.map((item) => {
-      console.log(mergedWishlist);
-      return (
-        <div className="makeupItemContainer">
-          <img src={item.image} className="makeupImages" />
-          <div className="makeupPrice">{item.price}</div>
-          <div className="makeupName">{item.name}</div>
-          <button
-            className="makeupRemoveButton"
-            onClick={() => removeMakeup(item)}
-          >
-            Remove Item
-          </button>
-        </div>
-      );
-    });
-  };
-
-  // <img src={item.product.image} className="makeupImages" />
-  //         <div className="makeupPrice">{item.product.price}</div>
-  //         <div className="makeupName">{item.product.name}</div>
-
-  let showMakeup = () => {
-    return makeupWishlist.map((item) => {
+  let displayWishlist = () => {
+    return wishlist.map((item) => {
       return (
         <div className="makeupItemContainer">
           <img src={item.product.image} className="makeupImages" />
-          <div className="makeupPrice">{item.product.price}</div>
           <div className="makeupName">{item.product.name}</div>
+          <div className="makeupPrice">${item.product.price}</div>
+
 
           <button
             className="makeupRemoveButton"
@@ -86,25 +36,6 @@ function Wishlist(props) {
       );
     });
   };
-
-  // let showFakestore = () => {
-  //   return fakestoreWishlist.map((item) => {
-  //     return (
-  //       <div className="fakestoreItemContainer">
-  //         <img src={item.product.image} className="fakestoreImages" />
-  //         <div className="fakestoreTitle">{item.product.title}</div>
-  //         <div className="fakestorePrice">{item.product.price}</div>
-
-  //         <button
-  //           className="fakestoreRemoveButton"
-  //           onClick={() => removeFakestore(item)}
-  //         >
-  //           Remove Item
-  //         </button>
-  //       </div>
-  //     );
-  //   });
-  // };
 
   let removeMakeup = (item) => {
     axios
@@ -112,16 +43,8 @@ function Wishlist(props) {
       .then(() => refreshMakeupPage());
   };
 
-  const clearAll = (item) => {
-    return mergedWishlist.map((item) => {
-      axios
-        .delete(`https://ironrest.herokuapp.com/wishlist2/${item._id}`)
-      //.then(() => refreshFakestorePage());
-    });
-  };
-
-  const clearAllMakeup = () => {
-    return makeupWishlist.map((item) => {
+  const clearWishlist = () => {
+    return wishlist.map((item) => {
       console.log(item);
       axios
         .delete(`https://ironrest.herokuapp.com/wishlist/${item._id}`)
@@ -129,51 +52,61 @@ function Wishlist(props) {
     });
   };
 
+
+
+
+
+
+
+  // need to change the if statement
   let wishlistQuantity = () => {
-    if (fakestoreWishlist.length > 0 || makeupWishlist.length > 0) {
-      return "Quantity:  " + (fakestoreWishlist.length + makeupWishlist.length);
-    } else {
-      return "Add some products!!!";
-    }
+    // if (fakestoreWishlist.length > 0 || wishlist.length > 0) {
+    //   return "Quantity:  " + (fakestoreWishlist.length + wishlist.length);
+    // } else {
+    //   return "Add some products!!!";
+    // }
   };
 
-  //counting price
-  const fakestoreTotalSum = () => {
-    return fakestoreWishlist.reduce(
-      (sum, item) => sum + Number(item.product.price),
-      0
-    );
-  };
 
+
+
+  // Calculate Sum of all items
   const makeupTotalSum = () => {
-    return makeupWishlist.reduce(
+    return wishlist.reduce(
       (sum, item) => sum + Number(item.product.price),
       0
     );
   };
 
+  // Display Sum of all items
   let totalSum = (item) => {
-    if (fakestoreWishlist.length > 0 || makeupWishlist.length > 0) {
+    if (wishlist.length > 0 || wishlist.length > 0) {
       return (
         "Total:  $" +
-        (Number(fakestoreTotalSum()) + Number(makeupTotalSum())).toFixed(2)
-      );
+        (Number(makeupTotalSum())).toFixed(2)
+      )
+    } else {
+      return (
+        "Total:  $0"
+      )
     }
   };
 
-  //sorting
+
+
+  // Sorting by Highest Price
   function sortByHigh() {
-    console.log(mergedWishlist);
-    setMergedWishlist(
-      [...mergedWishlist].sort((a, b) => {
+    setWishlist(
+      [...wishlist].sort((a, b) => {
         return Number(b.price) - Number(a.price);
       })
     );
   }
 
+  // Sorting by Lowest Price
   function sortByLow() {
-    setMergedWishlist(
-      [...mergedWishlist].sort((a, b) => {
+    setWishlist(
+      [...wishlist].sort((a, b) => {
         return Number(a.price) - Number(b.price);
       })
     );
@@ -184,18 +117,18 @@ function Wishlist(props) {
       <div>
         <button
           onClick={() => {
-            clearAllMakeup();
+            clearWishlist();
           }}
         >
           Clear All
         </button>
         <button onClick={sortByHigh}>Highest Price</button>
-
+        <button onClick={sortByLow}>Lowest Price</button>
         <span className="wishlistTotal">{wishlistQuantity()}</span>
         <span className="wishlistSum">{totalSum()}</span>
       </div>
 
-      {showMakeup()}
+      {displayWishlist()}
     </div>
   );
 }
