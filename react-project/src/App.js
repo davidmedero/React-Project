d542b6df32bcbc9cafb0432556b7c8375093e188
Cynthia Navarro <cynthianavarro93@hotmail.com>
2021-04-29 17:18:01 -0400

    add banners

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -5,12 +5,9 @@ import { Switch, Route } from "react-router-dom";
 import FakeStore from "./components/FakeStore";
 import Makeup from "./components/Makeup";
 import NewItem from "./components/NewItem";
-<<<<<<< HEAD
 import Games from "./components/Games";
-=======
 import NewStore from "./components/newStore";
 import ToyCarStore from "./components/ToyCarStore";
->>>>>>> de8cc172d0eb5262880c2f9f945213c118cf83af
 
 function App() {
   return (
@@ -20,12 +17,9 @@ function App() {
         <Route exact path="/fakestore" component={FakeStore} />
         <Route exact path="/Makeup" component={Makeup} />
         <Route exact path="/NewItem" component={NewItem} />
-<<<<<<< HEAD
         <Route exact path="/Games" component={Games}/>
-=======
         <Route exact path="/NewStore" component={NewStore} />
         <Route exact path="/ToyCarStore" component={ToyCarStore} />
->>>>>>> de8cc172d0eb5262880c2f9f945213c118cf83af
       </Switch>
     </div>
   );
