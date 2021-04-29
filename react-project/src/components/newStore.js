80b1da946859b534d2b1ccba9edf99a7e04f4651
Jesper <jesperbennett@protonmail.com>
2021-04-29 17:05:36 -0400

    wasd

--- react-project/src/components/newStore.js
+++ react-project/src/components/newStore.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import axios from "axios";
 
 function NewStore(props) {
   let [newProduct, setNewProduct] = useState({ product: {} });
+  let [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://ironrest.herokuapp.com/accessoriesStore`
+      )
+      .then((res) => {
+        setProducts(res.data);
+      });
+  }, []);
 
   function handleChange(e) {
     e.persist();
@@ -16,36 +27,78 @@ function NewStore(props) {
     axios.post("https://ironrest.herokuapp.com/accessoriesStore", newProduct);
   }
 
+
+  let displayAllProducts = () => {
+    return products.map((item, i) => {
+      return (
+        <div className="toyCar-container">
+          <img src={item.product.image} className="toyCar-img" />
+          <div className="toyCar-name">
+            <b>{item.product.name}</b>
+          </div>
+          <div className="toyCar-price">${item.product.price}</div>
+          <button
+            className="toyCar-button"
+            onClick={() => removeProduct(item.product)}
+          >
+            Remove
+      </button>
+        </div>
+      );
+    });
+  };
+
+
+  //Reloads the page
+  function refreshPage() {
+    axios.get(`https://ironrest.herokuapp.com/accessoriesStore`).then((res) => {
+      setProducts(res.data);
+    });
+  }
+
+  //Removes single product from Wishlist
+  let removeProduct = (item) => {
+    axios
+      .delete(`https://ironrest.herokuapp.com/accessoriesStore/${item._id}`)
+      .then(() => refreshPage());
+  };
+
   //Display on screen
   return (
-    <div className="newProduct-mainContainer">
-      <div className="navbar">
-        <Navbar />
-        <h2>Push Product to NEW STORE</h2>
+    <div>
+      <div className="newProduct-mainContainer">
+        <div className="navbar">
+          <Navbar />
+          <h2>Push Product to NEW STORE</h2>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <div className="neItemt-nameContainer">
+            <label>Product Name:</label> <br />
+            <input type="text" name="name" onChange={handleChange}></input> <br />
+          </div>
+
+          <div className="newItem-priceContainer">
+            <label>Price:</label> <br />
+            <input type="text" name="price" onChange={handleChange} />
+            <br />
+          </div>
+
+          <div className="newItem-priceContainer">
+            <label>Image URL:</label> <br />
+            <input type="text" name="image" onChange={handleChange} />
+            <br />
+          </div>
+
+          <button className="newItem-button" type="submit" value="Submit">
+            Add New
+        </button>
+        </form>
       </div>
 
-      <form onSubmit={handleSubmit}>
-        <div className="neItemt-nameContainer">
-          <label>Product Name:</label> <br />
-          <input type="text" name="name" onChange={handleChange}></input> <br />
-        </div>
-
-        <div className="newItem-priceContainer">
-          <label>Price:</label> <br />
-          <input type="text" name="price" onChange={handleChange} />
-          <br />
-        </div>
-
-        <div className="newItem-priceContainer">
-          <label>Image URL:</label> <br />
-          <input type="text" name="image" onChange={handleChange} />
-          <br />
-        </div>
-
-        <button className="newItem-button" type="submit" value="Submit">
-          Add New
-        </button>
-      </form>
+      <div>
+        {displayAllProducts()}
+      </div>
     </div>
   );
 }
